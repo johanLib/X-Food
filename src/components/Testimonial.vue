188f60affbc9f777a6131cae758e50ee493ebb32
Anass <seghiranas3@gmail.com>
2024-06-17 00:35:06 +0100

    fix: resolve issue with testimonial images in TestimonialComponent

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,14 +1,39 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { instersectionView } from '@/lib/script';
 import ratings from '@/data/ratings.json';
+
+import user1 from '@/assets/images/users/User1.webp';
+import user2 from '@/assets/images/users/User2.webp';
+import user3 from '@/assets/images/users/User3.webp';
+import user4 from '@/assets/images/users/User4.webp';
+import user5 from '@/assets/images/users/User5.webp';
+import khabib from '@/assets/images/users/Khabib.webp';
+import david from '@/assets/images/users/David-Goggins.webp';
+import huberman from '@/assets/images/users/Huberman.webp';
 
 const testimonialContainer = ref(null);
 const current = ref(0);
 const data = ref(ratings);
 
+const imageMap = {
+  user1: user1,
+  user2: user2,
+  user3: user3,
+  user4: user4,
+  user5: user5,
+  khabib: khabib,
+  david: david,
+  huberman: huberman
+};
+
+const userImage = computed(() => {
+  const imageName = data.value[current.value].userimage.toLowerCase();
+  return imageMap[imageName] || '';
+});
+
 onMounted(() => {
-    instersectionView(testimonialContainer);
+  instersectionView(testimonialContainer);
 });
 
 const Prev = () => {
@@ -37,7 +62,7 @@ const Next = () => {
 
     <div class="testimonial-right">
       <div class="testimonial-img">
-        <img :src="data[current].userimage" loading="lazy" width="500px" height="500px" alt="">
+        <img :src="userImage" loading="lazy" width="500px" height="500px" alt="">
       </div>
       <h3>{{ data[current].fullname }}</h3> <!-- FullName -->
       <span>{{ data[current].location }}</span> <!-- Basic Location -->
@@ -47,9 +72,3 @@ const Next = () => {
     </div>
   </section>
 </template>
-
-<script>
-export default {
-  name: 'TestimonialComponent'
-}
-</script>
